1a58cfab11cef6c7c6e3157d9bdab7a5d1e4bc0f
Eduardo García Sanz <edgarcia@vectorsf.com>
2015-09-09 11:09:16 +0200

    asking for local config on first run

--- config/index.js
+++ config/index.js
@@ -1,1 +1,9 @@
+var fs   = require('fs'),
+    path = require('path');
+
+if (!(process.env.SERVER || fs.existsSync(path.join(__dirname, 'local.js')))) {
+
+    throw new Error('Debes crear un archivo de configuración local en ' + __dirname + ', copia el de prod y sustituye sus valores.');
+}
+
 module.exports = require('./' + (process.env.SERVER || 'local'));
